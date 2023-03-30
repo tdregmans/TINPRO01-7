f637aae91337b52b06ab8419855c5e0b78c8c40c
Thijs Dregmans <92675694+tdregmans@users.noreply.github.com>
2023-03-30 21:23:47 +0200

    Update opdracht3.hs
    Final version

--- opdracht3.hs
+++ opdracht3.hs
@@ -23,6 +23,10 @@ dubbelen s = nub (s \\ (nub s))
 
 -- Opdracht 3.3
 
+
+allPossibilities :: [[Integer]]
+allPossibilities =  [[a,b,c,d,e]|a<-[1..6],b<-[1..6],c<-[1..6],d<-[1..6],e<-[1..6]]
+
 example :: [[Integer]]
 example = [[1,2,3,4,5],[2,2,2,2,2],[3,3,3,3,5],[4,4,4,5,1],[4,4,4,3,3],[3,5,4,3,5],[1,2,2,3,4],[6,5,4,3,2],[6,5,4,3,1]]
 
